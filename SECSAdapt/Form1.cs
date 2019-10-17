f44b12e9fe07d4a6da572870f869fbb9187e0313
Ethan <david19870709@hotmail.com>
2019-10-17 20:23:04 +0800

    first commit

---
+++ SECSAdapt/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TransferControl.CommandConvert;
+using TransferControl.Engine;
+using TransferControl.Management;
+
+namespace SECSAdapt
+{
+    public partial class Form1 : Form,IUserInterfaceReport
+    {
+        HostControl HostControl;
+        public Form1()
+        {
+            InitializeComponent();
+            HostControl = new HostControl(new SECSInterface.SECSGEM(this));
+        }
+
+        public void NewTask(string Id, TaskFlowManagement.Command TaskName, Dictionary<string, string> param = null)
+        {
+            HostControl.NewTask( Id,  TaskName,  param);
+        }
+
+        public void On_Alarm_Happen(AlarmInfo Alarm)
+        {
+           
+        }
+
+        public void On_Command_Error(Node Node, Transaction Txn, CommandReturnMessage Msg)
+        {
+            
+        }
+
+        public void On_Command_Excuted(Node Node, Transaction Txn, CommandReturnMessage Msg)
+        {
+           
+        }
+
+        public void On_Command_Finished(Node Node, Transaction Txn, CommandReturnMessage Msg)
+        {
+           
+        }
+
+        public void On_Command_TimeOut(Node Node, Transaction Txn)
+        {
+            
+        }
+
+        public void On_Connection_Error(string DIOName, string ErrorMsg)
+        {
+            
+        }
+
+        public void On_Connection_Status_Report(string DIOName, string Status)
+        {
+            
+        }
+
+        public void On_DIO_Data_Chnaged(string Parameter, string Value, string Type)
+        {
+            
+        }
+
+        public void On_Event_Trigger(Node Node, CommandReturnMessage Msg)
+        {
+            
+        }
+
+        public void On_Job_Location_Changed(Job Job)
+        {
+           
+        }
+
+        public void On_Message_Log(string Type, string Message)
+        {
+            
+        }
+
+        public void On_Node_Connection_Changed(string NodeName, string Status)
+        {
+          
+        }
+
+        public void On_Node_State_Changed(Node Node, string Status)
+        {
+           
+        }
+
+        public void On_Status_Changed(string Type, string Message)
+        {
+            
+        }
+
+        public void On_TaskJob_Aborted(TaskFlowManagement.CurrentProcessTask Task, string NodeName, string ReportType, string Message)
+        {
+           
+        }
+
+        public void On_TaskJob_Ack(TaskFlowManagement.CurrentProcessTask Task)
+        {
+            
+        }
+
+        public void On_TaskJob_Finished(TaskFlowManagement.CurrentProcessTask Task)
+        {
+           
+        }
+    }
+}
---
+++ SECSAdapt/HostControl.cs
@@ -0,0 +1,84 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TransferControl.Config;
+using TransferControl.Digital_IO.Comm;
+using TransferControl.Engine;
+using TransferControl.Management;
+
+namespace SECSAdapt
+{
+    public class HostControl: IConnectionReport
+    {
+        IUserInterfaceReport _Report;
+        static SocketClient socket;
+        public HostControl(IUserInterfaceReport Report)
+        {
+            _Report = Report;
+            socket = new SocketClient(this);
+            socket.Start();
+        }
+
+        public void NewTask(string Id, TaskFlowManagement.Command TaskName, Dictionary<string, string> param = null)
+        {
+            string Event = "NewTask";
+
+            socket.Send(JsonConvert.SerializeObject(new { Event, Id, TaskName, param }) + "\r");
+            
+        }
+
+        public void On_Connection_Connected(object Msg)
+        {
+            
+        }
+
+        public void On_Connection_Connecting(string Msg)
+        {
+            
+        }
+
+        public void On_Connection_Disconnected(string Msg)
+        {
+           
+        }
+
+        public void On_Connection_Error(string Msg)
+        {
+            
+        }
+
+        public void On_Connection_Message(object Msg)
+        {
+            JObject restoredObject = JsonConvert.DeserializeObject<JObject>(Msg.ToString());
+            //JObject可使用LINQ方式存取
+            //var q = from p in restoredObject.Properties()
+            //        where p.Name == "Name"
+            //        select p;
+
+            switch (restoredObject.Property("Event").Value.ToString())
+            {
+                case "SystemConfig":
+                    SystemConfig SysCfg = JsonConvert.DeserializeObject<SystemConfig>(restoredObject.Property("SystemConfig").Value.ToString());
+                    //Dictionary<string, string> param = new Dictionary<string, string>();
+                    //param.Add("@Target","ROBOT01");
+                    //NewTask("123123", TaskFlowManagement.Command.ROBOT_HOME,param);
+                    break;
+                case "On_TaskJob_Aborted":
+                    TaskFlowManagement.CurrentProcessTask Task = JsonConvert.DeserializeObject<TaskFlowManagement.CurrentProcessTask>(restoredObject.Property("Task").Value.ToString());
+                    string NodeName = restoredObject.Property("NodeName").Value.ToString();
+                    string ReportType = restoredObject.Property("ReportType").Value.ToString();
+                    string Message = restoredObject.Property("Message").Value.ToString();
+                    _Report.On_TaskJob_Aborted(Task, NodeName, ReportType, Message);
+                    break;
+                case "On_Alarm_Happend":
+                    AlarmInfo Alarm = JsonConvert.DeserializeObject<AlarmInfo>(restoredObject.Property("Alarm").Value.ToString());
+                    _Report.On_Alarm_Happen(Alarm);
+                    break;
+            }
+        }
+    }
+}
